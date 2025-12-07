26c1c1fcae80c71b03754e2b1c8f56e26707b773
not-a-mad-sayantist <rockistuart@gmail.com>
2025-12-07 10:35:12 +0530

    feat: Allow configurable server host and move API key to GitHub secrets.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = parseInt(process.env.PORT ?? '3000', 10);
-  await app.listen(PORT);
+  const HOST = process.env.HOST ?? '0.0.0.0';
+  await app.listen(PORT, HOST);
   console.log(`Server listening on port ${PORT}`);
 }
 bootstrap();
